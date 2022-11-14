87ea70590a1c11fddb329d27ceaa2f07b40cb40a
Henderson <hendalcosta@gmail.com>
2022-11-14 10:33:47 -0300

    Project Created

---
+++ backend/src/main/java/com/phantom/metasvendas/MetasvendasApplication.java
@@ -0,0 +1,13 @@
+package com.phantom.metasvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetasvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetasvendasApplication.class, args);
+	}
+
+}
